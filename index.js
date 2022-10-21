ef8bcdb806e20df3c9961494f0d19aec0cc8661a
JeganKannan1 <jegan@sparkouttech.com>
2022-10-21 13:04:03 +0530

    update and delete changes

--- index.js
+++ index.js
@@ -31,71 +31,70 @@ app.post('/create',async(req, res)=>{
     }
 })
 
-// app.get('/get',async(req,res)=>{
-//     try{
-//         const allUser = await User.find().select('-_id name').exec()
-//         if(allUser){
-//             return res.json({
-//                 data: allUser
-//             })
-//         }
-//     }
-//     catch(err){
-//         return res.json({error: err})
-//     }
-// })
+app.get('/get',async(req,res)=>{
+    try{
+        const allUser = await User.find({});
+        if(allUser){
+            return res.json({
+                data: allUser
+            })
+        }
+    }
+    catch(err){
+        return res.json({error: err})
+    }
+})
 
 
-// app.get('/get-by-id/:id',async(req,res)=>{
-//     try{
-//         const singleUser = await User.findById(req.params.id).exec()
-//         if(singleUser){
-//             return res.json({
-//                 data: singleUser
-//             })
-//         }
-//     }
-//     catch(err){
-//         return res.json({error: err})
-//     }
-// })
+app.get('/get-by-id/:id',async(req,res)=>{
+    try{
+        const singleUser = await User.findById(req.params.id).exec()
+        if(singleUser){
+            return res.json({
+                data: singleUser
+            })
+        }
+    }
+    catch(err){
+        return res.json({error: err})
+    }
+})
 
-// app.patch('/update-by-id/:id',async(req,res)=>{
-//     try{
-//         const id  = req.params.id
-//         const updates = {
-//             name : req.body.name
-//         }
-//         const options = {
-//             new: true
-//         }
-//         const singleUser = await User.findByIdAndUpdate(id,updates, options).exec()
-//         if(singleUser){
-//             return res.json({
-//                 data: singleUser
-//             })
-//         }
-//     }
-//     catch(err){
-//         return res.json({error: err})
-//     }
-// })
+app.post('/update-by-id',async(req,res)=>{
+    try{
+        const id  = req.body.id
+        const updates = {
+            name : req.body.name
+        }
+        const options = {
+            new: true
+        }
+        const singleUser = await User.findByIdAndUpdate(id,updates, options).exec()
+        if(singleUser){
+            return res.json({
+                data: singleUser
+            })
+        }
+    }
+    catch(err){
+        return res.json({error: err})
+    }
+})
 
-// app.delete('/delete-by-id/:id',async(req,res)=>{
-//     try{
-//         const id  = req.params.id
-
-//         const singleUser = await User.findByIdAndDelete({_id: id}).exec()
-//         if(singleUser){
-//             return res.json({
-//                 data: "success"
-//             })
-//         }
-//     }
-//     catch(err){
-//         return res.json({error: err})
-//     }
-// })
+app.get('/delete-by-id/:id',async(req,res)=>{
+    try{
+        const id  = req.params.id
+        const singleUser = await User.findByIdAndDelete(id).exec()
+        if(singleUser){
+            return res.json({
+                data: "success"
+            })
+        }
+    }
+    catch(err){
+        return res.json({error: err})
+    }
+})
 
 
 app.listen(port,()=>{
